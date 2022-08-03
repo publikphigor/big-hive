c513d205dcc3d9e4e1912ae6d7ab482ef981a343
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-08-03 06:49:09 +0000

    Implemented dark mode and scrolling gallery.

--- js/main.js
+++ js/main.js
@@ -9,31 +9,47 @@
 const navLinks = document.querySelectorAll("nav ul li");
 const mobileNavbar = document.querySelector(".mobile-navbar");
 const mobileNavbarInner = document.querySelector(".mobile-navbar-inner");
+const mobileNavbarBtn = document.querySelector(".mobile-toggles__navbar");
 const navBtn = document.querySelector(".burger");
-const overlay = document.querySelector(".overlay");
 const root = document.documentElement;
+const themeBtns = document.querySelectorAll(".theme-switch");
 const scrollToTopBtn = document.querySelector(".scroll-to-top");
+
+/*
+
+===================================== FUNCTIONS ===============================================
+
+*/
 
 function setDarkTheme() {
   root.style.setProperty("--background", "#121212");
-  root.style.setProperty("--black", "#ffffff");
-  root.style.setProperty("--white", "#000000");
-  root.style.setProperty("--text-color", "#959595");
+  root.style.setProperty("--text-color", "#ffffff");
+  themeBtns.forEach((btn) => {
+    btn.querySelector("ion-icon").name = "partly-sunny-outline";
+  });
 }
 
 function setLightTheme() {
-  root.style.setProperty("--background", "#fafafa");
-  root.style.setProperty("--black", "#000000");
-  root.style.setProperty("--white", "#ffffff");
-  root.style.setProperty("--text-color", "#4d4d4d");
+  root.style.setProperty("--background", "#e9ab17");
+  root.style.setProperty("--text-color", "#000000");
+  themeBtns.forEach((btn) => {
+    btn.querySelector("ion-icon").name = "moon-outline";
+  });
 }
 
 function openAndCloseNavbar() {
   mobileNavbar.classList.toggle("active");
-  navBtn.classList.toggle("active");
-  overlay.classList.toggle("active");
+  mobileNavbarBtn.classList.toggle("active");
 }
 
+/*
+
+===================================== EVENT HANDLERS ===============================================
+
+*/
+
+// Navbar toggle
+mobileNavbarBtn.addEventListener("click", openAndCloseNavbar);
 // animate hover state on navbar links (desktop)
 function showPointer(links) {
   links.forEach((link) => {
@@ -50,4 +66,19 @@ showPointer(navLinks);
 // scroll to top
 scrollToTopBtn.addEventListener("click", function (e) {
   window.scrollTo(0, 0);
+});
+
+// Theme toggle
+themeBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.classList.contains("light")) {
+      setDarkTheme();
+      btn.classList.remove("light");
+      btn.classList.add("dark");
+    } else {
+      setLightTheme();
+      btn.classList.remove("dark");
+      btn.classList.add("light");
+    }
+  });
 });
